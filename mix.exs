ec403699b5724e02f928093d92cc17d1d996fcb1
Marco Polita <polita.marco@gmail.com>
2018-08-12 14:41:14 +0200

    Docs and last refinements

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      name: "Issues",
+      source_url: "https://github.com/efesto/issues",
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript_config()
     ]
   end
 
@@ -21,10 +24,16 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:httpoison, "~> 1.0"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.18.1"},
+      {:earmark, "~> 1.2.4"}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
